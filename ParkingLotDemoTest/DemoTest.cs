7e1dc01b064e4554bfc11e7e9258f41b12a49137
shiwei.fu <shiwei.fu@thoughtworks.com>
2022-10-31 11:37:13 +0800

    feat: init project

---
+++ ParkingLotDemoTest/DemoTest.cs
@@ -0,0 +1,13 @@
+namespace ParkingLotDemoTest;
+
+using Xunit;
+
+public class DemoTest
+{
+    [Fact]
+    public void Should_return_true_when_test_success()
+    {
+        Assert.True(true);
+    }
+
+}
